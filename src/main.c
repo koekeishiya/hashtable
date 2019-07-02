54312dc4d0a76727ce178ac32c9501cf2625ef56
koekeishiya <aasvi93@hotmail.com>
2019-07-02 12:49:14 +0200

    update

--- src/main.c
+++ src/main.c
@@ -3,58 +3,70 @@
 #define HASHTABLE_IMPLEMENTATION
 #include "hashtable.h"
 
-int compare_string(const char *a, const char *b)
+static TABLE_HASH_FUNC(hash_string)
 {
-    if(a && b) {
-        while(*a && *b && *a == *b) {
-            ++a;
-            ++b;
-        }
-        return *a == '\0' && *b == '\0';
-    }
-    return 0;
-}
+    unsigned long hash = 0;
+    unsigned long high = 0;
+    char *k = key;
 
-unsigned long hash_string(char *key)
-{
-    unsigned long hash = 0, high;
-    while(*key) {
-        hash = (hash << 4) + *key++;
+    while (*k) {
+        hash = (hash << 4) + *k++;
         high = hash & 0xF0000000;
-        if(high) {
+
+        if (high) {
             hash ^= (high >> 24);
         }
+
         hash &= ~high;
     }
+
     return hash;
 }
+
+static TABLE_COMPARE_FUNC(compare_string)
+{
+    char *a = key_a;
+    char *b = key_b;
+
+    while(*a && *b && *a == *b) {
+        ++a;
+        ++b;
+    }
+
+    return *a == '\0' && *b == '\0';
+}
+
+struct point
+{
+    float x, y;
+};
 
 int main()
 {
     struct table table;
-    int *value;
+    struct point *value;
 
     const char *keys[] =  { "key1", "key2", "key3" };
-    int values[3] = { 5, 6, 7 };
+    struct point values[3] = { {5,5}, {6,6}, {7,7} };
 
-    table_init(&table, 128, (table_hash_func)hash_string, (table_compare_func)compare_string);
+    table_init(&table, 128, hash_string, compare_string);
 
     for(int i = 0; i < 3; ++i) {
-        table_add(&table, (void*)keys[i], (void*)&values[i]);
+        table_add(&table, &keys[i], &values[i]);
     }
 
     for(int i = 0; i < 3; ++i) {
-        value = table_find(&table, (void*)keys[i]);
-        printf("key '%s' -> %d\n", keys[i], *value);
+        value = table_find(&table, &keys[i]);
+        printf("key '%s' -> %f, %f\n", keys[i], value->x, value->y);
     }
 
-    table_remove(&table, "key1");
-    table_remove(&table, "key2");
+    table_remove(&table, &keys[0]);
+    table_remove(&table, &keys[1]);
 
     for(int i = 0; i < 3; ++i) {
-        value = table_find(&table, (void*)keys[i]);
+        value = table_find(&table, &keys[i]);
         if(value) {
-            printf("key '%s' -> %d\n", keys[i], *value);
+            printf("key '%s' -> %f, %f\n", keys[i], value->x, value->y);
         } else {
             printf("key '%s' -> <not found>\n", keys[i]);
         }
